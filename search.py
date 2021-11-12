9d6e647f4814805a05f940d25adc371a8d285580
bmedcalf34 <bmedcalf2@student.gsu.edu>
2021-11-12 17:55:03 +0000

    First commit with functioning API key. Search.py generates random joke as example

--- search.py
+++ search.py
@@ -1,20 +1,11 @@
+import os
+from dotenv import find_dotenv, load_dotenv
 import spoonacular as sp
 from pprint import pprint
 
-api = sp.API()
-# query = input("Enter ingredient:")
-# # Parse an ingredient # str | The (natural language) search query. (optional)
-# number = 10  # int | The maximum number of items to return (between 1 and 100). Defaults to 10. (optional) (default to 10)# bool | Whether to return more meta information about the ingredients. (optional)
-# intolerances = "egg"
-# api_response = api.autocomplete_ingredient_search(
-#     query=query,
-#     number=number,
-#     intolerances=intolerances,
-# )
-# pprint(api_response)
-# # response = api.parse_ingredients(ingredient, servings=1)
-# # data = response.json()
-# # print(data[0]["name"])
+load_dotenv(find_dotenv())
+api = sp.API(os.getenv("API_KEY"))
+
 response = api.get_a_random_food_joke()
 data = response.json()
 print(data["text"])
