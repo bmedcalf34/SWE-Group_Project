484e15606f32f4c52ce3961c525b48903672a399
bmedcalf34 <bmedcalf2@student.gsu.edu>
2021-11-14 17:18:16 +0000

    Created search function to hold recipe search algo

--- search.py
+++ search.py
@@ -3,14 +3,17 @@ from dotenv import find_dotenv, load_dotenv
 import spoonacular as sp
 from pprint import pprint
 
-load_dotenv(find_dotenv())
-api = sp.API(os.getenv("API_KEY"))
-query = input("Input an ingredient:")
-number = int(input("How many recipes would you like to see?"))
 
-response = api.autocomplete_recipe_search(query=query, number=number)
-data = response.json()
-for i in range(number):
-    # print(data[i]["name"])
-    pprint(data[i]["title"])
-    pprint(data[i]["id"])
+def recipe_search():
+    load_dotenv(find_dotenv())
+    api = sp.API(os.getenv("API_KEY"))
+    query = input("Input an ingredient:")
+    number = int(input("How many recipes would you like to see?"))
+
+    response = api.autocomplete_recipe_search(query=query, number=number)
+    data = response.json()
+    for i in range(number):
+        recipe_name = data[i]["title"]
+        recipe_id = data[i]["id"]
+        pprint(recipe_name, recipe_id)
+    return recipe_name, recipe_id
