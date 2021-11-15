f21298ecb386450666a5adcca75aa881411fae01
Maryam Botrus <Mbotrus1@student.gsu.edu>
2021-11-14 19:38:50 -0500

    Working Login System
    The login system now works - and allows user sign up and login. Additional functionality is needed to save preferences.

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from flask import render_template
 import os
 import time
 import spoonacular as sp
-
+from flask_login import UserMixin
+from flask_login import login_user, current_user, LoginManager
 
 
 if os.sys.platform == "win32":
@@ -50,18 +51,24 @@ db = SQLAlchemy(app)
 '''
 Data Base Import 
 '''
-class Users(db.Model):
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<Person %r>' % self.username
+        return f"<User {self.username}>"
+
+    def get_username(self):
+        return self.username
+
+db.create_all()
+db.session.commit()
 
 @app.route("/")
 def main_page():
     # serves the main page of the application
-    return render_template("index.html")
+    return render_template("index.html",login_name="empty_user",login_status=False)
 
 
 @app.route("/calculator")
@@ -137,12 +144,64 @@ def recipes():
 def diets():
     return render_template("diet_selection.html")
 
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/signup", methods=["POST"])
+def signup_post():
+    
+    
+    if request.method == "POST":
+        username = flask.request.form.get("username")
+        password = flask.request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        if user:
+            pass
+        else:
+            user = User(username=username, password=password)
+            db.session.add(user)
+            db.session.commit()
+
+    return render_template("index.html",login_name=user.username,login_status=True)
+
+
+@app.route("/login")
+def login():
+    return flask.render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    username = flask.request.form.get("username")
+    print(username)
+    password = flask.request.form.get("password")
+    print(password)
+    users = User.query.filter_by(username=username).all()
+    condition = False
+    for user in users:
+        print(user.username)
+        print(user.password)
+        if(user.password==password):
+            condition=True
+    print(condition)
+    if condition:
+        return render_template("index.html",login_name=user.username,login_status=True)
+    else:
+        return flask.jsonify({"status": 401, "reason": "Username or Password Error"})
 
 
 #allows the user to search for different meal or food options
 @app.route("/meals")
 def meal_search():
     return render_template("meal_search.html")
+
+
+
+'''
+App Sign Up Code 
+'''
 
 if __name__ == "__main__":
     app.run()
