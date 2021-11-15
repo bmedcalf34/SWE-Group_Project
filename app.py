d546f6910385181b5a181ebf82156f158a9898ae
asaw1 <asaw1@student.gsu.edu>
2021-11-15 18:02:57 -0500

    add nutrition.py

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov  9 21:10:10 2021
-
-@author: Maryam Botrus
-"""
 import flask
-from flask import Flask
-import requests
+from flask import Flask, request
 from flask import render_template
 import spoonacular as sp
 import os
@@ -16,24 +9,49 @@ from flask_login.utils import login_required
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+import time
+from nutrition import nutrition
+load_dotenv(find_dotenv())
+if os.sys.platform == "win32":
+    time.clock = time.time
+else:
+    _timer = time.time
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+SP_KEY = []
+try:
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    SP_KEY = os.getenv("API_KEY")
+except:
+    print("Unable to Resolve Client keys")
+# fixes windows specific parsing error for environment files
+try:
+    if DATABASE_URL == None or SP_KEY == None:
+        f = open(".env", "r")
+        temp = f.read()
+        DATABASE_URL = temp.splitlines()[0].split(" ")[2][1:-1]
+        SP_KEY = temp.splitlines()[1].split(" ")[2][1:-1]
+        f.close()
+except:
+    print("No env file found")
 
-
-db_url = os.getenv("DATABASE_URL")
-if db_url.startswith("postgres://"):
-    db_url = db_url.replace("postgres://", "postgresql://", 1)
-app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 # Gets rid of a warning
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = b"I am a secret key!"  # don't defraud my app ok?
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 
-class User(UserMixin, db.Model):
+'''
+Data Base Import 
+'''
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
-    password = db.Column(db.String(80))
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
         return f"<User {self.username}>"
@@ -41,26 +59,13 @@ class User(UserMixin, db.Model):
     def get_username(self):
         return self.username
 
-
-
-login_manager = LoginManager()
-login_manager.login_view = "login"
-login_manager.init_app(app)
-
-
-@login_manager.user_loader
-def load_user(user_name):
-    return User.query.get(user_name)
+db.create_all()
+db.session.commit()
 
 @app.route("/")
 def main_page():
     # serves the main page of the application
-    return render_template("index.html")
-
-
-"""
-tabulates food costs
-"""
+    return render_template("index.html",login_name="empty_user",login_status=False)
 
 
 @app.route("/calculator")
@@ -68,77 +73,63 @@ def food_costs():
     return render_template("calculator.html")
 
 
-"""
- provides nutritional information on food options
- consider building api logic in a seperate class 
-"""
-
-
-@app.route("/nutrition")
+ #provides nutritional information on food options
+ #consider building api logic in a seperate class 
+@app.route("/nutrition", methods=["GET", "POST"])
 def nutrition():
-    load_dotenv(find_dotenv())
-    # nutrition of food
-    nutrients_name = []
-    nutrients_amount = []
-    nutrients_unit = []
-    api = sp.API(os.getenv("API_KEY"))
+   
+    
+    
     food = "chicken"
-    # find the ingredient id
-    response = api.autocomplete_ingredient_search(
-        f"{food}", number=1, metaInformation=True
-    )
-    data = response.json()
+    amount = 1
+        
+    if request.method == "POST":
+        food = request.form["ingredients"]
+        amount = request.form["amount"]
+    
 
-    food_id = data[0]["id"]
-    # find the nutrtion information using id and the amount
-    response_for_nutrtition = api.get_food_information(f"{food_id}", amount=1)
-    nutrition_data = response_for_nutrtition.json()
-    nutrients = nutrition_data["nutrition"]["nutrients"]
+    
+    datas = nutrition(food)
 
-    # loop through and append nutrients data into
-    for i in range(len(nutrients)):
-        nutrients_name.append((nutrients[i]["name"]))
-        nutrients_amount.append((nutrients[i]["amount"]))
-        nutrients_unit.append((nutrients[i]["unit"]))
+    return render_template("nutrition.html",
+                data = datas["data[0]"],
+                nutrient_name = datas["nutrient_name"],
+                nutrients_amount = datas["nutrient_amount"],
+                nutrients_unit = datas["nutrient_unit"],
+                len = len(datas["nutrient_amount"]),
+                
+     )
 
-    return render_template(
-        "nutrition.html",
-        data=data[0],
-        nutrient_name=nutrients_name,
-        nutrients_amount=nutrients_amount,
-        nutrients_unit=nutrients_unit,
-        len=len(nutrients),
-    )
-
-
-@app.route("/recipes")
+@app.route("/recipes", methods=["GET", "POST"])
 def recipes():
+    print('Recipes served')
     return render_template("recipes.html")
-
 
 @app.route("/diet_selection")
 def diets():
     return render_template("diet_selection.html")
 
-
 @app.route("/signup")
 def signup():
-    return flask.render_template("signup.html")
+    return render_template("signup.html")
 
 
 @app.route("/signup", methods=["POST"])
 def signup_post():
-    username = flask.request.form.get("username")
-    password = flask.request.form.get("password")
-    user = User.query.filter_by(username=username).first()
-    if user:
-        pass
-    else:
-        user = User(username=username, password=password)
-        db.session.add(user)
-        db.session.commit()
+    
+    
+    if request.method == "POST":
+        username = flask.request.form.get("username")
+        password = flask.request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        if user:
+            pass
+        else:
+            user = User(username=username, password=password)
+            db.session.add(user)
+            db.session.commit()
 
-    return flask.redirect(flask.url_for("index"))
+    return render_template("index.html",login_name=user.username,login_status=True)
 
 
 @app.route("/login")
@@ -149,25 +140,33 @@ def login():
 @app.route("/login", methods=["POST"])
 def login_post():
     username = flask.request.form.get("username")
+    print(username)
     password = flask.request.form.get("password")
-    user = User.query.filter_by(username=username).first()
-    Database_password = user.password
-    if password == Database_password:
-        login_user(user)
-        return flask.redirect(flask.url_for("bp.nutrition"))
-
+    print(password)
+    users = User.query.filter_by(username=username).all()
+    condition = False
+    for user in users:
+        print(user.username)
+        print(user.password)
+        if(user.password==password):
+            condition=True
+    print(condition)
+    if condition:
+        return render_template("index.html",login_name=user.username,login_status=True)
     else:
         return flask.jsonify({"status": 401, "reason": "Username or Password Error"})
 
-"""
-allows the user to search for different meal or food options
-"""
 
-
+#allows the user to search for different meal or food options
 @app.route("/meals")
 def meal_search():
     return render_template("meal_search.html")
 
+
+
+'''
+App Sign Up Code 
+'''
 
 if __name__ == "__main__":
     app.run()
