36a080332e7784d2bcab3f32311eb72f99ce01aa
Shaunniel <90009014+Shaunniel@users.noreply.github.com>
2021-11-16 11:25:50 -0500

    Delete testing.py

--- testing.py
+++
@@ -1,7 +0,0 @@
-import requests
-from requests import api
-from pprint import pprint
-api_key = "660710df9210423ab9d19d84c8cef2a3"
-s_input = input("What do you want to search? ")
-response = requests.get(f"https://api.spoonacular.com/recipes/complexSearch?apiKey={api_key}&query={s_input}&addRecipeNutrition=True&number=9").json()
-print(response)
